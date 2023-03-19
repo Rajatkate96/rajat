e84b138ac6abdf130ee7ed0fe36dbda865d30296
Rajat Kate <rajatkate123@gmail.com>
2023-03-19 19:32:32 +0530

    json file path changed

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import numpy as np
 import json
 import pickle
 
-with open('artifacts\project_data.json','r') as file:
+with open('project_data.json','r') as file:
     project_data = json.load(file)
 
 
